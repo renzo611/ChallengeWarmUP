0abf2d6845202d37efb09e4cbea4cc71580e0a1e
Renzo Fajardo <renzofajardo613@gmail.com>
2021-10-21 19:17:04 -0300

    Agrego estructura básica del proyecto.

---
+++ src/test/java/com/alkemy/ChallengeWarmUP/ChallengeWarmUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alkemy.ChallengeWarmUP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeWarmUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
